b9037b6d946895ff7c0eb1c234cd4ab226671ad9
Tanya Schlusser <tanya@tickel.net>
2015-04-22 13:46:06 -0500

    improve the annualization equation

--- mpt/mpt.py
+++ mpt/mpt.py
@@ -13,7 +13,7 @@ from scipy.optimize import minimize
 
 def _get_efficient_frontier(
         expected_values, standard_deviations, covariances,
-        returns = [x * 0.15 / 42 for x in range(42)]):
+        returns = [x * 0.25 / 42 for x in range(42)]):
     cov_inv = invert(covariances)
     A = sum(sum(cov_inv))
     B = sum(np.dot(cov_inv, expected_values))
@@ -36,10 +36,10 @@ def get_efficient_frontier(df, annualized=False):
     N = len(expected_values)
 
     if not annualized:
-        days_in_year = 250
-        expected_values  = expected_values * days_in_year
-        standard_deviations = standard_deviations * days_in_year
-        covariances = covariances * days_in_year * days_in_year
+        days_in_year = 260
+        expected_values  = ((1+expected_values) ** days_in_year) - 1
+        standard_deviations = standard_deviations * math.sqrt(days_in_year)
+        covariances = covariances * days_in_year
 
     return _get_efficient_frontier(
             expected_values, standard_deviations, covariances)
@@ -47,6 +47,15 @@ def get_efficient_frontier(df, annualized=False):
 
 def get_portfolio(df, returns, lb=0, ub=1):
     """Get the best portfolio given the specified returns.
+
+    lb, ub are scalars or a vector with the same length as the number
+    of possible stocks in the portfolio.
+
+    lb = 0 means you cannot short a stock
+    ub = 1 means you have 100% of your portfolio in one stock
+
+    usually people fix the lb = 0 or lb = - 0.05 (no more than 5% shorted
+    per stock) and set ub for treasury bills to be like 0.03 or something.
     """
     ncol = df.shape()[1]
     expected_values  = df.mean()
@@ -68,13 +77,33 @@ def get_portfolio(df, returns, lb=0, ub=1):
 
     initial_guess = np.ones(ncol) / ncol
 
+    allocations = []
     risks = []
     for r in returns:
         # Constraint is A x = b ==> A x - b = 0
+        #
         # First constraint is sum(x) = 1 so portfolio totals to 100%
         # The next constraint is the target return --
         #  sum(x * expected_values) = target_return
-        A = np.array(np.ones((2,ncol)))
+        #
+        #  A = [1 1 1    b = [1
+        #       1 1 1]        desired_return ]
+        #
+        #
+        # --> so ...  A x = b   becomes   A x - b = 0
+        #
+        #   A           x          b
+        #   [1  1  1    [x1    -   [1                 =  [0
+        #    e1 e2 e3]   x2         desired_return]       0]
+        #                x3]
+        #
+        # xi = the percent allocation of each stock in the portfolio
+        # ei = the expected return for a given stock
+        # 
+        #     x1 +    x2 +    x3 = 1              (portfolio 100%)
+        #  e1 x1 + e2 x2 + e3 x3 = desired_return (expected return is as desried)
+        #
+        A = np.array(np.ones((2, ncol)))
         A[:,:-1] = expected_values
         b = np.array([[1.],[r]])
         constraints = dict(type='eq', fun=lambda x: A.dot(x) - b)
@@ -82,7 +111,13 @@ def get_portfolio(df, returns, lb=0, ub=1):
         result = minimize(objective, initial_guess, method='SLSQP',
                 jac=gradient, bounds=bounds, constraints=constraints)
 
-        risks.append(result.x if result.success else np.nan)
+        
+        allocations.append(list(result.x) if result.success else [np.nan] * ncol)
+        risks.append(result.x.dot(covariances.dot(result.x)))
 
-    return pd.DataFrame(dict(returns=returns, risks=risks))
+    result = np.zeros((len(returns), ncol + 2))
+    result[:,0] = returns
+    result[:,1] = risks
+    result[:,2:] = np.array(allocations)
+    return pd.DataFrame(result, columns = ["returns", "risks"] + list(df.names))
 
